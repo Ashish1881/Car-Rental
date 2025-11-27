7b43da9da6638e401586be485a8c4cc0b9670543
Ashish <ashish@gmail.com>
2025-11-27 13:45:53 +0530

    Car Rental booking System

---
+++ src/components/featuredSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function featuredSection() {
+  return (
+    <div className="flex flex-col items-center py-24 px-6 md:px-16 lg:px24 xl:px32"></div>
+  );
+}
+
+export default featuredSection;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import CarCard from "../components/CarCard";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <CarCard />
+    </div>
+  );
+}
+
+export default Home;
