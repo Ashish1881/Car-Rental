0a2fd6795e3a62af35637f3943bf21aa9d99eaa5
Ashish <ashish@gmail.com>
2025-11-28 12:49:15 +0530

    Banner section is complited

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
-import CarCard from "../components/CarCard";
 import FeaturedSection from "../components/featuredSection";
+import Banner from "../components/Banner";
 
 function Home() {
   return (
     <div>
       <Hero />
       <FeaturedSection />
+      <Banner />
     </div>
   );
 }
